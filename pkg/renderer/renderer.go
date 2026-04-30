24ccada329aa8fbabb63d4a52df1f96929e29607
fujidaiti <fujidaiti@gmail.com>
2026-04-30 09:32:48 +0900

    cache templates

--- pkg/renderer/renderer.go
+++ pkg/renderer/renderer.go
@@ -7,11 +7,38 @@ import (
 )
 
 func RenderTemplate(w http.ResponseWriter, templateFile string) {
-	template, _ := template.ParseFiles("templates/"+templateFile, "templates/base.layout.tmpl")
-	err := template.Execute(w, nil)
+	tmpl, err := templateFor(templateFile)
 	if err != nil {
 		fmt.Println("Something went wrong: ", err)
-		errorTemplate, _ := template.ParseFiles("templates/error.page.tmpl")
-		errorTemplate.Execute(w, nil)
+		tmpl, _ = templateFor("templates/error.page.tmpl")
 	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Something went wrong: ", err)
+		w.Write([]byte("Oops, something went wrong..."))
+	}
+}
+
+var cache = make(map[string]*template.Template)
+
+func templateFor(templateFile string) (*template.Template, error) {
+	tmpl, cached := cache[templateFile]
+	if !cached {
+		files := []string{
+			fmt.Sprintf("templates/%s", templateFile),
+			"templates/base.layout.tmpl",
+		}
+
+		var err error
+		tmpl, err = template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[templateFile] = tmpl
+		fmt.Println("Cache template: ", templateFile)
+	}
+
+	return tmpl, nil
 }
