eef98b6486914def96f1cbd8d069e91482723e74
fujidaiti <fujidaiti@gmail.com>
2026-05-02 19:57:02 +0900

    refactor routing to use chi

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/fujidaiti/bookings/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
-	http.ListenAndServe(portNumber, nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", handlers.Home)
+	r.Get("/about", handlers.About)
+	http.ListenAndServe(portNumber, r)
 }
