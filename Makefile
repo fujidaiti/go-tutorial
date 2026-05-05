08fa94e73207aed822db28cc7a5276a316aa8a43
fujidaiti <fujidaiti@gmail.com>
2026-05-05 15:02:04 +0900

    add db seeding script

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
-.PHONY: db-migrate
+include .env
 
+.PHONY: db-migrate
 db-migrate:
-	env $(cat .env | xargs) go run ./cmd/migrate
+	env $$(cat .env | xargs) go run ./cmd/migrate
+
+.PHONY: db-seed
+db-seed:
+	psql -h $(PGHOST) -p $(PGPORT) -U $(PGUSER) -d $(PGDATABASE) -f seed.sql
+
+.PHONY: db-reset
+db-reset:
+	dropdb -h $(PGHOST) -p $(PGPORT) --if-exists $(PGDATABASE)
+	createdb -h $(PGHOST) -p $(PGPORT) $(PGDATABASE)
+	$(MAKE) db-migrate
+	$(MAKE) db-seed
---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO rooms (name) VALUES
+    ('General''s - King Suite, Ocean View, Private Bath'),
+    ('General''s - King Suite, Garden View, Private Bath'),
+    ('General''s - Queen Room, Ocean View, Private Bath'),
+    ('General''s - Queen Room, Garden View, Shared Bath'),
+    ('General''s - Twin Room, Ocean View, Private Bath'),
+    ('Major''s - Twin Room, Garden View, Private Bath'),
+    ('Major''s - Twin Room, Garden View, Shared Bath'),
+    ('Major''s - Single Room, Ocean View, Private Bath'),
+    ('Major''s - Single Room, Garden View, Private Bath'),
+    ('Major''s - Single Room, Garden View, Shared Bath');
